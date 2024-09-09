68251fd89ff62bba5004a4dc07da0b2c8c98101e
Rômulo Lopes <romulo.lopes@ifce.edu.br>
2024-09-09 11:56:24 -0300

    Update autokary2022.py

--- configs/solov2/autokary2022.py
+++ configs/solov2/autokary2022.py
@@ -1,9 +1,17 @@
-_base_ = './solov2_r50_fpn_1x_coco.py'
+_base_ = './solov2-light_r50_fpn_ms-3x_coco.py'
 
-# We also need to change the num_classes in head to match the dataset's annotation
+# model settings
 model = dict(
-    roi_head=dict(
-        bbox_head=dict(num_classes=25), mask_head=dict(num_classes=25)))
+    backbone=dict(
+        dcn=dict(type='DCNv2', deformable_groups=1, fallback_on_stride=False),
+        stage_with_dcn=(False, True, True, True)),
+    mask_head=dict(
+        feat_channels=256,
+        stacked_convs=3,
+        scale_ranges=((1, 64), (32, 128), (64, 256), (128, 512), (256, 2048)),
+        mask_feature_head=dict(out_channels=128),
+        dcn_cfg=dict(type='DCNv2'),
+        dcn_apply_to_all_conv=False))  # light solov2 head
 
 # Modify dataset related settings
 data_root = 'data/autokary2022/'
