2a28e0c558ac88ed0076608836f997454e8e5406
Rômulo Lopes <romulo.lopes@ifce.edu.br>
2024-09-10 18:56:48 -0300

    Update autokary2022.py

--- configs/mask_rcnn/autokary2022.py
+++ configs/mask_rcnn/autokary2022.py
@@ -45,9 +45,9 @@ import os
 from mmdet.engine.hooks import MlflowLoggerHook
 dagshub_uri = os.environ.get('DAGSHUB_MLFLOW')
 
-custom_hooks = dict(
-    logger=dict(type=MlflowLoggerHook, interval=50 , exp_name="Nome Teste",uri=dagshub_uri )
-)
+#custom_hooks = dict(
+#    logger=dict(type=MlflowLoggerHook, interval=50 , exp_name="Nome Teste",uri=dagshub_uri )
+#)
 
 # We can use the pre-trained Mask RCNN model to obtain higher performance
 load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
