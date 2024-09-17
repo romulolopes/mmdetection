8384aa4cb0d08fde2043e6f37266aa20ad6b0b76
Rômulo Lopes <romulo.lopes@ifce.edu.br>
2024-09-17 23:43:35 +0000

    coco_metrics add try catch

--- configs/faster_rcnn/cariotipo.py
+++ configs/faster_rcnn/cariotipo.py
@@ -7,7 +7,7 @@ _base_ = [
 
 load_from = 'https://cdn-model.openxlab.org.cn/models%2Fweight%2Fmmdetection%2FFaster+R-CNN%2Ffaster_rcnn_r50_caffe_dc5_1x_coco_20201030_151909-531f0f43.pth?Expires=1726599141&OSSAccessKeyId=LTAI5tCdKkrGqdpR7PDyejq7&Signature=JlEGhYKgFV4h6%2Fj%2B4UFK%2F47Mz4A%3D&response-content-disposition=attachment%3B%20filename%3Dfaster_rcnn_r50_caffe_dc5_1x_coco_20201030_151909-531f0f43.pth'
 
-'''
+
 mlflow_tags = {
         "model_type": "Faster RCNN",
         "dataset": "Cariotipo",
@@ -15,4 +15,4 @@ mlflow_tags = {
 custom_hooks = [
     dict(type='MlflowLoggerHook',  exp_name="Faster RCNN" , params=mlflow_tags)
 ]
-'''
+
