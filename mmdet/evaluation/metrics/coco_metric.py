a5981c4c88e32aed9b4fe3268d6d1682b9b12780
Rômulo Lopes <romulo.lopes@ifce.edu.br>
2024-09-17 23:40:42 +0000

    coco_metrics add try catch

--- mmdet/evaluation/metrics/coco_metric.py
+++ mmdet/evaluation/metrics/coco_metric.py
@@ -240,7 +240,10 @@ class CocoMetric(BaseMetric):
                 data['image_id'] = image_id
                 data['bbox'] = self.xyxy2xywh(bboxes[i])
                 data['score'] = float(scores[i])
-                data['category_id'] = self.cat_ids[label]
+                try:
+                    data['category_id'] = self.cat_ids[label]
+                except Exception :
+                    continue
                 bbox_json_results.append(data)
 
             if segm_json_results is None:
