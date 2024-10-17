7814f8878b4feec91e58e849c4630bfa25a239a2
Rômulo Lopes <romulo.lopes@ifce.edu.br>
2024-10-17 11:04:03 -0300

    Update autokary2022.py

--- configs/instaboost/autokary2022.py
+++ configs/instaboost/autokary2022.py
@@ -1,6 +1,6 @@
 _base_ = [
-  '../_base_/datasets/autokary2022.py'
-  'mask-rcnn_r50_fpn_instaboost-4x_coco.py', 
+  '../_base_/datasets/autokary2022.py',
+  'mask-rcnn_r50_fpn_instaboost-4x_coco.py'
 ]
 
 load_from = 'https://download.openmmlab.com/mmdetection/v2.0/instaboost/mask_rcnn_r50_fpn_instaboost_4x_coco/mask_rcnn_r50_fpn_instaboost_4x_coco_20200307-d025f83a.pth'
